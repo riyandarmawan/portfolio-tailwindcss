6433256541c1e016b8e859cf1bc346573307280c
Riyan042 <riyandarmawan042@gmail.com>
2024-03-10 19:52:41 +0700

    Home and About section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-        container: {
-            center: true,
-            padding: "16px",
-        },
-        extend: {
-            colors: {
-                primary: "#0ea5e9",
-                secondary: "#64748b",
-            },
-            screens: {
-                "2xl": "1320px",
-            },
-        },
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
     },
-    plugins: [],
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        secondary: "#64748b",
+        dark: "#020617",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
 };
